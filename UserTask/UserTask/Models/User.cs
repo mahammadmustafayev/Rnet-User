49a1dc9e411b921737e2e22a4e4db0ae71d225b2
mmustafayev213-3 <mustafamehmed1251@gmail.com>
2023-05-04 15:07:18 +0400

    added user task

---
+++ UserTask/UserTask/Models/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserTask.Interfaces;
+
+namespace UserTask.Models
+{
+    internal class User : IAccount
+    {
+        private static int _id = 1;
+        private string _password;
+        public int Id { get => _id++; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get => _password; set
+            {
+                if (PasswordChecker(value))
+                {
+                    _password = value;
+                }
+            }
+        }
+        public User(string fullName,string email)
+        {
+            this.FullName = fullName;
+            this.Email = email;
+        }
+        public bool PasswordChecker(string password)
+        {
+            bool isLower=false;
+            bool isDigit=false;
+            bool isUpper=false;
+            if (password.Length>8)
+            {
+                foreach (var character in password)
+                {
+                    if (!char.IsDigit(character)) isDigit = false;
+                    else if (!char.IsLower(character)) isLower = false;
+                    else if (char.IsUpper(character)) isUpper = false;
+                }
+            }
+            else
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("Password null gele bilmez");
+                Console.ResetColor();
+                return false;
+            }
+            if (isDigit && isLower && isUpper) 
+            {
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.WriteLine("Password uqurludur");
+                Console.ResetColor();
+                return true;
+            } 
+            return false;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Id:{_id} FullName:{FullName} Email:{Email}");
+        }
+    }
+}
---
+++ UserTask/UserTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using UserTask.Models;
+
+namespace UserTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new(
+                   fullName:"Mahammad Mustafayev",
+                   email:"mahammadmustafayev@gmail.com"
+                );
+            user.Password = "password";
+            user.ShowInfo();
+
+            User user1 = new(
+                fullName:"Valeh Amirbekov",
+                email: "valehamirbekov@gmail.com"
+                );
+            user1.Password = "pasS1word";
+            user1.ShowInfo();
+        }
+    }
+}
